c1c35674db447d43dad547b385e53414d8ecfe43
patyukin <me@patyukin.ru>
2024-06-09 14:25:38 +0300

    hw-5

--- hw-5/main.go
+++ hw-5/main.go
@@ -1,63 +1,78 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
-	"sync"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-func RunProcessor(wg *sync.WaitGroup, prices []map[string]float64, mu *sync.Mutex) {
-	go func() {
-		defer wg.Done()
-		mu.Lock()
-		defer mu.Unlock()
-		for _, price := range prices {
-			for key, value := range price {
-				price[key] = value + 1
-			}
-			fmt.Println(price)
-		}
-	}()
-}
-
-func RunWriter() <-chan map[string]float64 {
-	prices := make(chan map[string]float64)
-	go func() {
-		currentPrice := map[string]float64{
-			"inst1": 1.1,
-			"inst2": 2.1,
-			"inst3": 3.1,
-			"inst4": 4.1,
-		}
-
-		for i := 1; i < 5; i++ {
-			temp := make(map[string]float64)
-			for key, value := range currentPrice {
-				temp[key] = value + 1
-			}
-			prices <- temp
-		}
-		close(prices)
-	}()
-	return prices
-}
+const (
+	permission = 0o600
+	fileName   = "input.txt"
+)
 
 func main() {
-	p := RunWriter()
-	prices := make([]map[string]float64, 0)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ch := make(chan string)
 
-	for price := range p {
-		prices = append(prices, price)
+	go readInput(ctx, ch)
+	go writeToFile(ch)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	fmt.Printf("\nПриложение завершено.\n")
+}
+
+func readInput(ctx context.Context, ch chan<- string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Завершение ввода...")
+			close(ch)
+
+			return
+		default:
+			if scanner.Scan() {
+				ch <- scanner.Text()
+			}
+		}
+	}
+}
+
+func writeToFile(ch <-chan string) {
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, permission)
+	if err != nil {
+		fmt.Println("Ошибка открытия файла:", err)
+		return
 	}
 
-	for _, price := range prices {
-		fmt.Println(price)
-	}
+	defer func(file *os.File) {
+		err = file.Close()
+		if err != nil {
+			fmt.Println("Ошибка закрытия файла:", err)
+		}
+	}(file)
 
-	mu := &sync.Mutex{}
-	wg := &sync.WaitGroup{}
-	wg.Add(3)
-	RunProcessor(wg, prices, mu)
-	RunProcessor(wg, prices, mu)
-	RunProcessor(wg, prices, mu)
-	wg.Wait()
+	writer := bufio.NewWriter(file)
+	defer func(writer *bufio.Writer) {
+		err = writer.Flush()
+		if err != nil {
+			fmt.Println("Ошибка записи в файл:", err)
+		}
+	}(writer)
+
+	for text := range ch {
+		_, err = writer.WriteString(text + "\n")
+		if err != nil {
+			fmt.Println("Ошибка записи в файл:", err)
+			return
+		}
+	}
 }
